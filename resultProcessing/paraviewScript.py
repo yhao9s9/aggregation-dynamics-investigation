f396f017707265544a6bc6da352705e167e25179
yhao <y.hao@uva.nl>
2023-10-04 11:18:20 +0200

    update paraview script

--- resultProcessing/paraviewScript.py
+++ resultProcessing/paraviewScript.py
@@ -1,5 +1,4 @@
 # trace generated using paraview version 5.9.0
-# This is a script to process the simulation results in paraview
 
 #### import the simple module from the paraview
 from paraview.simple import *
@@ -7,19 +6,16 @@ from paraview.simple import *
 paraview.simple._DisableFirstRenderCameraReset()
 
 # get active source.
-solid_128_0000_000vtu = GetActiveSource()
-
-# Properties modified on solid_128_0000_000vtu
-solid_128_0000_000vtu.TimeArray = 'None'
+solidstl = GetActiveSource()
 
 # get active view
 renderView1 = GetActiveViewOrCreate('RenderView')
 
 # show data in view
-solid_128_0000_000vtuDisplay = Show(solid_128_0000_000vtu, renderView1, 'UnstructuredGridRepresentation')
+solidstlDisplay = Show(solidstl, renderView1, 'GeometryRepresentation')
 
 # trace defaults for the display properties.
-solid_128_0000_000vtuDisplay.Representation = 'Surface'
+solidstlDisplay.Representation = 'Surface'
 
 # reset view to fit data
 renderView1.ResetCamera()
@@ -27,24 +23,27 @@ renderView1.ResetCamera()
 # get the material library
 materialLibrary1 = GetMaterialLibrary()
 
-# show color bar/color legend
-solid_128_0000_000vtuDisplay.SetScalarBarVisibility(renderView1, True)
+# find source
+model_128pvd = FindSource('model_128.pvd')
+
+# show data in view
+model_128pvdDisplay = Show(model_128pvd, renderView1, 'UnstructuredGridRepresentation')
+
+# trace defaults for the display properties.
+model_128pvdDisplay.Representation = 'Surface'
 
 # update the view to ensure updated data information
 renderView1.Update()
 
-# get color transfer function/color map for 'Label'
-labelLUT = GetColorTransferFunction('Label')
-
-# get opacity transfer function/opacity map for 'Label'
-labelPWF = GetOpacityTransferFunction('Label')
+# set active source
+SetActiveSource(model_128pvd)
 
 # create a new 'Calculator'
-calculator1 = Calculator(registrationName='Calculator1', Input=solid_128_0000_000vtu)
+calculator1 = Calculator(registrationName='Calculator1', Input=model_128pvd)
 
 # Properties modified on calculator1
-calculator1.ResultArrayName = 'PointZ'
-calculator1.Function = 'coordsZ'
+calculator1.ResultArrayName = 'velocity'
+calculator1.Function = 'ux*iHat+uy*jHat+uz*kHat'
 
 # show data in view
 calculator1Display = Show(calculator1, renderView1, 'UnstructuredGridRepresentation')
@@ -53,10 +52,108 @@ calculator1Display = Show(calculator1, renderView1, 'UnstructuredGridRepresentat
 calculator1Display.Representation = 'Surface'
 
 # hide data in view
-Hide(solid_128_0000_000vtu, renderView1)
+Hide(model_128pvd, renderView1)
+
+# update the view to ensure updated data information
+renderView1.Update()
+
+# create a query selection
+QuerySelect(QueryString='(volume_factor == 0)', FieldType='POINT', InsideOut=1)
+
+# set active source
+SetActiveSource(calculator1)
+
+# create a new 'Extract Selection'
+extractSelection1 = ExtractSelection(registrationName='ExtractSelection1', Input=calculator1)
+
+# show data in view
+extractSelection1Display = Show(extractSelection1, renderView1, 'UnstructuredGridRepresentation')
+
+# trace defaults for the display properties.
+extractSelection1Display.Representation = 'Surface'
+
+# hide data in view
+Hide(calculator1, renderView1)
+
+# update the view to ensure updated data information
+renderView1.Update()
+
+# set active source
+SetActiveSource(solidstl)
+
+# create a new 'Transform'
+transform1 = Transform(registrationName='Transform1', Input=solidstl)
+
+# Properties modified on transform1.Transform
+transform1.Transform.Scale = [0.001, 0.001, 0.001]
+
+# show data in view
+transform1Display = Show(transform1, renderView1, 'GeometryRepresentation')
+
+# trace defaults for the display properties.
+transform1Display.Representation = 'Surface'
+
+# hide data in view
+Hide(solidstl, renderView1)
+
+# update the view to ensure updated data information
+renderView1.Update()
+
+# reset view to fit data
+renderView1.ResetCamera()
+
+# set active source
+SetActiveSource(extractSelection1)
+
+# toggle 3D widget visibility (only when running from the GUI)
+Hide3DWidgets(proxy=transform1.Transform)
+
+# create a new 'Point Dataset Interpolator'
+pointDatasetInterpolator1 = PointDatasetInterpolator(registrationName='PointDatasetInterpolator1', Input=extractSelection1,
+    Source=transform1)
+
+# Properties modified on pointDatasetInterpolator1
+pointDatasetInterpolator1.Kernel = 'GaussianKernel'
+
+# Properties modified on pointDatasetInterpolator1.Kernel
+pointDatasetInterpolator1.Kernel.Radius = 0.001
+
+# get active view
+renderView1 = GetActiveViewOrCreate('RenderView')
+
+# show data in view
+pointDatasetInterpolator1Display = Show(pointDatasetInterpolator1, renderView1, 'GeometryRepresentation')
+
+# trace defaults for the display properties.
+pointDatasetInterpolator1Display.Representation = 'Surface'
+
+# hide data in view
+Hide(extractSelection1, renderView1)
+
+# hide data in view
+Hide(transform1, renderView1)
+
+# update the view to ensure updated data information
+renderView1.Update()
+
+# create a new 'Calculator'
+calculator2 = Calculator(registrationName='Calculator2', Input=pointDatasetInterpolator1)
+
+# Properties modified on calculator2
+calculator2.ResultArrayName = 'PointZ'
+calculator2.Function = 'coordsZ'
+
+# show data in view
+calculator2Display = Show(calculator2, renderView1, 'GeometryRepresentation')
+
+# trace defaults for the display properties.
+calculator2Display.Representation = 'Surface'
+
+# hide data in view
+Hide(pointDatasetInterpolator1, renderView1)
 
 # show color bar/color legend
-calculator1Display.SetScalarBarVisibility(renderView1, True)
+calculator2Display.SetScalarBarVisibility(renderView1, True)
 
 # update the view to ensure updated data information
 renderView1.Update()
@@ -67,94 +164,25 @@ pointZLUT = GetColorTransferFunction('PointZ')
 # get opacity transfer function/opacity map for 'PointZ'
 pointZPWF = GetOpacityTransferFunction('PointZ')
 
-# create a new 'Calculator'
-calculator2 = Calculator(registrationName='Calculator2', Input=calculator1)
-
-# Properties modified on calculator2
-calculator2.ResultArrayName = 'velocity'
-calculator2.Function = 'ux*iHat+uy*jHat+uz*kHat'
-
-# show data in view
-calculator2Display = Show(calculator2, renderView1, 'UnstructuredGridRepresentation')
-
-# trace defaults for the display properties.
-calculator2Display.Representation = 'Surface'
-
-# hide data in view
-Hide(calculator1, renderView1)
-
-# show color bar/color legend
-calculator2Display.SetScalarBarVisibility(renderView1, True)
-
-# update the view to ensure updated data information
-renderView1.Update()
-
 # create a query selection
 QuerySelect(QueryString='(PointZ  == max(PointZ))', FieldType='POINT', InsideOut=0)
 
-# set active source
-SetActiveSource(calculator2)
-
 # create a new 'Extract Selection'
-extractSelection1 = ExtractSelection(registrationName='ExtractSelection1', Input=calculator2)
+extractSelection2 = ExtractSelection(registrationName='ExtractSelection2', Input=calculator2)
 
 # Get index information for maximum coordZ point
-pointD = paraview.servermanager.Fetch(extractSelection1)
+pointD = paraview.servermanager.Fetch(extractSelection2)
 xValue = pointD.GetPoint(0)[0]
 yValue = pointD.GetPoint(0)[1]
 maxZ = pointD.GetPoint(0)[2]
-Zvalue = maxZ*2/3
-
-# show data in view
-extractSelection1Display = Show(extractSelection1, renderView1, 'UnstructuredGridRepresentation')
-
-# trace defaults for the display properties.
-extractSelection1Display.Representation = 'Surface'
-
-# hide data in view
-Hide(calculator2, renderView1)
-
-# show color bar/color legend
-extractSelection1Display.SetScalarBarVisibility(renderView1, True)
-
-# update the view to ensure updated data information
-renderView1.Update()
-
-# hide data in view
-Hide(extractSelection1, renderView1)
+zValuePercent = maxZ*2/3
+zValueFix = maxZ-0.002
 
 # set active source
 SetActiveSource(calculator2)
 
-# show data in view
-calculator2Display = Show(calculator2, renderView1, 'UnstructuredGridRepresentation')
-
-# show color bar/color legend
-calculator2Display.SetScalarBarVisibility(renderView1, True)
-
-# reset view to fit data
-renderView1.ResetCamera()
-
-# create a new 'Extract Surface'
-extractSurface1 = ExtractSurface(registrationName='ExtractSurface1', Input=calculator2)
-
-# show data in view
-extractSurface1Display = Show(extractSurface1, renderView1, 'GeometryRepresentation')
-
-# trace defaults for the display properties.
-extractSurface1Display.Representation = 'Surface'
-
-# hide data in view
-Hide(calculator2, renderView1)
-
-# show color bar/color legend
-extractSurface1Display.SetScalarBarVisibility(renderView1, True)
-
-# update the view to ensure updated data information
-renderView1.Update()
-
 # create a new 'Clip'
-clip1 = Clip(registrationName='Clip1', Input=extractSurface1)
+clip1 = Clip(registrationName='Clip1', Input=calculator2)
 
 # Properties modified on clip1.ClipType
 clip1.ClipType.Origin = [xValue, yValue, 0.0001]
@@ -167,10 +195,22 @@ clip1Display = Show(clip1, renderView1, 'UnstructuredGridRepresentation')
 clip1Display.Representation = 'Surface'
 
 # hide data in view
-Hide(extractSurface1, renderView1)
+Hide(calculator2, renderView1)
 
 # show color bar/color legend
 clip1Display.SetScalarBarVisibility(renderView1, True)
+
+# show data in view
+extractSelection2Display = Show(extractSelection2, renderView1, 'UnstructuredGridRepresentation')
+
+# trace defaults for the display properties.
+extractSelection2Display.Representation = 'Surface'
+
+# hide data in view
+Hide(calculator2, renderView1)
+
+# show color bar/color legend
+extractSelection2Display.SetScalarBarVisibility(renderView1, True)
 
 # update the view to ensure updated data information
 renderView1.Update()
@@ -179,7 +219,7 @@ renderView1.Update()
 clip2 = Clip(registrationName='Clip2', Input=clip1)
 
 # Properties modified on clip2.ClipType
-clip2.ClipType.Origin = [xValue, yValue, Zvalue]
+clip2.ClipType.Origin = [xValue, yValue, zValuePercent]
 clip2.ClipType.Normal = [0.0, 0.0, -1.0]
 
 # show data in view
@@ -210,8 +250,8 @@ Show3DWidgets(proxy=clip1.ClipType)
 clip3 = Clip(registrationName='Clip3', Input=clip1)
 
 # Properties modified on clip3.ClipType
-clip3.ClipType.Origin = [xValue, yValue, Zvalue]
-clip3.ClipType.Normal = [0.0, -1.0, 0.0]
+clip3.ClipType.Origin = [xValue, yValue, zValueFix]
+clip3.ClipType.Normal = [0.0, 0.0, -1.0]
 
 # show data in view
 clip3Display = Show(clip3, renderView1, 'UnstructuredGridRepresentation')
@@ -241,7 +281,7 @@ Show3DWidgets(proxy=clip1.ClipType)
 clip4 = Clip(registrationName='Clip4', Input=clip1)
 
 # Properties modified on clip4.ClipType
-clip4.ClipType.Origin = [xValue, yValue, Zvalue]
+clip4.ClipType.Origin = [xValue, yValue, zValuePercent]
 clip4.ClipType.Normal = [0.0, 1.0, 0.0]
 
 # show data in view
@@ -255,6 +295,37 @@ Hide(clip1, renderView1)
 
 # show color bar/color legend
 clip4Display.SetScalarBarVisibility(renderView1, True)
+
+# update the view to ensure updated data information
+renderView1.Update()
+
+# set active source
+SetActiveSource(clip1)
+
+# toggle 3D widget visibility (only when running from the GUI)
+Hide3DWidgets(proxy=clip4.ClipType)
+
+# toggle 3D widget visibility (only when running from the GUI)
+Show3DWidgets(proxy=clip1.ClipType)
+
+# create a new 'Clip'
+clip5 = Clip(registrationName='Clip5', Input=clip1)
+
+# Properties modified on clip5.ClipType
+clip5.ClipType.Origin = [xValue, yValue, zValuePercent]
+clip5.ClipType.Normal = [0.0, -1.0, 0.0]
+
+# show data in view
+clip5Display = Show(clip5, renderView1, 'UnstructuredGridRepresentation')
+
+# trace defaults for the display properties.
+clip5Display.Representation = 'Surface'
+
+# hide data in view
+Hide(clip1, renderView1)
+
+# show color bar/color legend
+clip5Display.SetScalarBarVisibility(renderView1, True)
 
 # update the view to ensure updated data information
 renderView1.Update()
@@ -274,22 +345,22 @@ spreadSheetView1.ColumnToSort = ''
 spreadSheetView1.BlockSize = 1024
 
 # show data in view
-clip4Display_1 = Show(clip4, spreadSheetView1, 'SpreadSheetRepresentation')
+clip5Display_1 = Show(clip5, spreadSheetView1, 'SpreadSheetRepresentation')
 
 # assign view to a particular cell in the layout
 AssignViewToLayout(view=spreadSheetView1, layout=layout1, hint=2)
 
 # show data in view
-calculator2Display_1 = Show(calculator2, spreadSheetView1, 'SpreadSheetRepresentation')
+extractSelection1Display_1 = Show(extractSelection1, spreadSheetView1, 'SpreadSheetRepresentation')
 
 # Properties modified on spreadSheetView1
 spreadSheetView1.HiddenColumnLabels = []
 
 # Properties modified on spreadSheetView1
-spreadSheetView1.HiddenColumnLabels = ['Point ID', 'PointZ', 'Points', 'Points_Magnitude', 'coefficient', 'elongation_rate', 'fx', 'fy', 'fz', 'intensity', 'permeability', 'pressure', 'rt', 'shear_rate', 'shear_stress', 'ux', 'uy', 'uz', 'velocity', 'velocity_Magnitude', 'volume_factor', 'Block Number']
+spreadSheetView1.HiddenColumnLabels = ['Point ID', 'vtkOriginalPointIds', 'Points', 'Points_Magnitude', 'coefficient', 'elongation_rate', 'fx', 'fy', 'fz', 'intensity', 'permeability', 'pressure', 'rt', 'shear_rate', 'shear_stress', 'ux', 'uy', 'uz', 'velocity', 'velocity_Magnitude', 'volume_factor', 'Block Number']
 
 # Properties modified on spreadSheetView1
-spreadSheetView1.HiddenColumnLabels = ['Point ID', 'PointZ', 'Points', 'Points_Magnitude', 'coefficient', 'elongation_rate', 'fx', 'fy', 'fz', 'intensity', 'permeability', 'pressure', 'rt', 'shear_rate', 'shear_stress', 'ux', 'uy', 'uz', 'velocity', 'volume_factor', 'Block Number']
+spreadSheetView1.HiddenColumnLabels = ['Point ID', 'vtkOriginalPointIds', 'Points', 'Points_Magnitude', 'coefficient', 'elongation_rate', 'fx', 'fy', 'fz', 'intensity', 'permeability', 'pressure', 'rt', 'shear_rate', 'shear_stress', 'ux', 'uy', 'uz', 'velocity', 'volume_factor', 'Block Number']
 
 # export view
 ExportView('/Users/yhao/Downloads/volume.csv', view=spreadSheetView1)
@@ -298,13 +369,13 @@ ExportView('/Users/yhao/Downloads/volume.csv', view=spreadSheetView1)
 clip1Display_1 = Show(clip1, spreadSheetView1, 'SpreadSheetRepresentation')
 
 # Properties modified on spreadSheetView1
-spreadSheetView1.HiddenColumnLabels = ['Point ID', 'PointZ', 'Points', 'Points_Magnitude', 'coefficient', 'fx', 'fy', 'fz', 'intensity', 'permeability', 'pressure', 'rt', 'shear_rate', 'shear_stress', 'ux', 'uy', 'uz', 'velocity', 'volume_factor', 'Block Number']
+spreadSheetView1.HiddenColumnLabels = ['Point ID', 'vtkOriginalPointIds', 'Points', 'Points_Magnitude', 'coefficient', 'fx', 'fy', 'fz', 'intensity', 'permeability', 'pressure', 'rt', 'shear_rate', 'shear_stress', 'ux', 'uy', 'uz', 'velocity', 'volume_factor', 'Block Number']
 
 # Properties modified on spreadSheetView1
-spreadSheetView1.HiddenColumnLabels = ['Point ID', 'PointZ', 'Points', 'Points_Magnitude', 'coefficient', 'fx', 'fy', 'fz', 'intensity', 'permeability', 'pressure', 'rt', 'shear_stress', 'ux', 'uy', 'uz', 'velocity', 'volume_factor', 'Block Number']
+spreadSheetView1.HiddenColumnLabels = ['Point ID', 'vtkOriginalPointIds', 'Points', 'Points_Magnitude', 'coefficient', 'fx', 'fy', 'fz', 'intensity', 'permeability', 'pressure', 'rt', 'shear_stress', 'ux', 'uy', 'uz', 'velocity', 'volume_factor', 'Block Number']
 
 # Properties modified on spreadSheetView1
-spreadSheetView1.HiddenColumnLabels = ['Point ID', 'PointZ', 'Points', 'Points_Magnitude', 'coefficient', 'fx', 'fy', 'fz', 'intensity', 'permeability', 'pressure', 'rt', 'ux', 'uy', 'uz', 'velocity', 'volume_factor', 'Block Number']
+spreadSheetView1.HiddenColumnLabels = ['Point ID', 'vtkOriginalPointIds', 'Points', 'Points_Magnitude', 'coefficient', 'fx', 'fy', 'fz', 'intensity', 'permeability', 'pressure', 'rt', 'ux', 'uy', 'uz', 'velocity', 'volume_factor', 'Block Number']
 
 # export view
 ExportView('/Users/yhao/Downloads/surface.csv', view=spreadSheetView1)
@@ -313,19 +384,25 @@ ExportView('/Users/yhao/Downloads/surface.csv', view=spreadSheetView1)
 clip2Display_1 = Show(clip2, spreadSheetView1, 'SpreadSheetRepresentation')
 
 # export view
-ExportView('/Users/yhao/Downloads/top.csv', view=spreadSheetView1)
+ExportView('/Users/yhao/Downloads/toppercent.csv', view=spreadSheetView1)
 
 # show data in view
 clip3Display_1 = Show(clip3, spreadSheetView1, 'SpreadSheetRepresentation')
 
 # export view
-ExportView('/Users/yhao/Downloads/backward.csv', view=spreadSheetView1)
+ExportView('/Users/yhao/Downloads/topfix.csv', view=spreadSheetView1)
 
 # show data in view
 clip4Display_1 = Show(clip4, spreadSheetView1, 'SpreadSheetRepresentation')
 
 # export view
 ExportView('/Users/yhao/Downloads/faceward.csv', view=spreadSheetView1)
+
+# show data in view
+clip5Display_1 = Show(clip5, spreadSheetView1, 'SpreadSheetRepresentation')
+
+# export view
+ExportView('/Users/yhao/Downloads/backward.csv', view=spreadSheetView1)
 
 #================================================================
 # addendum: following script captures some of the application
@@ -336,15 +413,16 @@ ExportView('/Users/yhao/Downloads/faceward.csv', view=spreadSheetView1)
 # saving layout sizes for layouts
 
 # layout/tab size in pixels
-layout1.SetSize(1453, 1094)
+layout1.SetSize(1449, 1094)
 
 #-----------------------------------
 # saving camera placements for views
 
 # current camera placement for renderView1
-renderView1.CameraPosition = [0.08366339653730392, 0.036315194331109524, 0.08017969658442585]
-renderView1.CameraFocalPoint = [0.08366339653730392, 0.036315194331109524, 0.0032228081472567283]
-renderView1.CameraParallelScale = 0.019917908379369333
+renderView1.CameraPosition = [0.16380422772901648, 0.03608805313706398, 0.003643254263326526]
+renderView1.CameraFocalPoint = [0.08370302990078926, 0.03608805313706398, 0.003643254263326526]
+renderView1.CameraViewUp = [0.0, 0.0, 1.0]
+renderView1.CameraParallelScale = 0.020731715533469877
 
 #--------------------------------------------
 # uncomment the following to render all views
